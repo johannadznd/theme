f511ac1484352669c08acd07d47054a1efaa6b10
Johanna Dézarnaud <johanna.dezarnaud@orange.fr>
2020-07-01 14:06:13 +0200

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,5 +4,30 @@
     {
     dynamic_sidebar('sidebar');
     } 
+    else{?>
+    <style>
+    .aside {
+    width: 0vw;
+    border:none;
+    }
+    .left{
+        width: 96vw !important ;
+    }
+    p{
+        max-width: unset;
+    }
+    #description {
+    width: 95vw;
+    margin: 0;
+    }
+    #contenue2 {
+    width: 70vw;
+    }
+    #pagination1 {
+    margin-left: 40vw;
+    }
+    </style>    
+    <?php
+    }
     ?>
 </div>
